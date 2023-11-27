c106a8e9ec73fbce2b3438abe2b0975671f57a8d
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-11-27 23:06:11 +0200

    home work

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,63 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[];
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+        this.tenants = [];
+    }
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if(this.door) {
+            this.tenants.push(person);
+            console.log('person entered the house')
+        }
+        else {
+            console.log('Door is closed')
+        }
+    }
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    openDoor(key: Key): void {
+        if(key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Door is opened')
+        } else {
+            console.log('The key is wrong, door is closed')
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
